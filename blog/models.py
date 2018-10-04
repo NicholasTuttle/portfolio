e49fb2b7e96e66f206080f7101540775b7a2a230
Nicholas Tuttle <nicholastuttle@outlook.com>
2018-10-04 16:20:44 -0400

    add a default for each field

--- blog/models.py
+++ blog/models.py
@@ -2,10 +2,12 @@ from django.db import models
 
 
 # Create your models here.
+
+
 class Blog(models.Model):
-    title = models.CharField(max_length=200),
-    pub_date = models.DateTimeField(),
-    body = models.TextField(),
-    image = models.ImageField(upload_to='images/'),
+    title = models.CharField(max_length=200, null=True, blank=True)
+    pub_date = models.DateTimeField(null=True, blank=True)
+    body = models.TextField(null=True, blank=True)
+    image = models.ImageField(upload_to='images/', null=True, blank=True)
 
 
