708387582db08cf63aa5b858360b07849bce114f
Nicholas Tuttle <nicholastuttle@outlook.com>
2018-10-30 09:21:44 -0400

    adding static image problem

--- blog/models.py
+++ blog/models.py
@@ -10,4 +10,9 @@ class Blog(models.Model):
     body = models.TextField(null=True, blank=True)
     image = models.ImageField(upload_to='images/', null=True, blank=True)
 
+    def summary(self):
+        return self.body[:200]
 
+    def __str__(self):
+        return self.title
+    # this controls how your blog posts are titled when viewing models in Django admin
